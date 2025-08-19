78f4df6f0976254ad0a667a0804fa09278676572
okotdesderio59-crypto <okotdesderio59@gmail.com>
2025-08-19 16:34:01 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;WB00wZ7I#qz5kE1QC5FAGsT7OlnWFZvvbwmS6dwJUtpJH4fIJIXU"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
