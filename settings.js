f3b8fe17753612c0b214cb63c29c7d84ae38479b
okotdesderio59-crypto <okotdesderio59@gmail.com>
2025-08-19 16:47:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;WB00wZ7I#qz5kE1QC5FAGsT7OlnWFZvvbwmS6dwJUtpJH4fIJIXU"
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;jJlyjazQ#lS4mho3lvwFUywl4zbPOW-TwioNURJ3NUQ76YMMZaZc"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
